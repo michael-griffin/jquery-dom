e6d8671f69ea1580e5e0bf1cb4befc1c82af355f
Christopher Alley <calleyviola@gmail.com>
2023-08-14 20:43:13 -0400

    Add sort function

--- movie-app/app.js
+++ movie-app/app.js
@@ -19,8 +19,8 @@ function handleForm(evt) {
     .text( `${$title} ${$rating} `)
     .append($button); */
   const $listItem = $("<li>")
-    .html( `<span class="movie-title">${$title}</span>` +
-    `<span class="movie-rating"> ${$rating}`)
+    .html( `<span class="title-span">${$title}</span>` +
+    `<span class="rating-span"> ${$rating}`)
     .append($button);
   $(".movie-reviews").append($listItem);
 }
@@ -46,7 +46,36 @@ function handleAlphabetSort(){
   //Possible ways to sort:
   //select all list elements, get values, re add to dom?
 
-  $listItems = $('li').val(); //maybe use .toArray();
+  const $titles = $('.title-span'); //maybe use .toArray();
+  const $ratings = $('.rating-span');
+
+  const movieObj = {}
+
+  for (let i = 0; i < $titles.length; i++) {
+    movieObj[$titles[i].innerText] = $ratings[i].innerText;
+  }
+
+  const sortedTitles = Object.keys(movieObj)
+    .map(x => x.toLowerCase())
+    .sort();
+  console.log('sorted titles is', sortedTitles)
+
+  $('ul').empty();
+  const $button = $("<button>").text("X");
+
+
+/*   const $listItem = $("<li>")
+    .text( `${$title} ${$rating} `)
+    .append($button); */
+
+for (let title of sortedTitles) {
+  const $listItem = $("<li>")
+  .html( `<span class="title-span">${title}</span>` +
+  `<span class="rating-span"> ${movieObj[title]}`)
+  .append($button);
+$(".movie-reviews").append($listItem);
+}
+
 
   //sort $listItems by title
   //transform
